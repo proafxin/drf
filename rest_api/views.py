0ab287b87d88c3135bd53a8d48cda5e852503f60
Masum Billal <2643770+proafxin@users.noreply.github.com>
2021-03-24 04:59:26 +0600

    added function view for article list and generic views for article and author list

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.http import (
+    HttpResponse,
+    JsonResponse,
+)
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+)
+from rest_framework.generics import (
+    ListAPIView,
+    ListCreateAPIView,
+)
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+
+from rest_api.models import (
+    Author,
+    Article,
+)
+from rest_api.model_serializers import (
+    AuthorSerializer,
+    ArticleSerializer,
+)
+
+
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        data = serializer.data
+        json_response = JsonResponse(data=data, safe=False)
+
+        return json_response
+    
+    if request.method == 'POST':
+        parser = JSONParser()
+        data = parser.parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            json_response = JsonResponse(serializer.data, status=201)
+        else:
+            json_response = JsonResponse(serializer.errors, status=400)
+        
+        return json_response
+
+class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [
+        IsAdminUser,
+        IsAuthenticated,
+        IsAuthenticatedOrReadOnly,
+    ]
+
+class AuthorList(ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsAuthenticated,
+        IsAdminUser,
+    ]
