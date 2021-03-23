626568d0b6f0fdc4fb0e50d7dd3151d0a9ccc485
Masum Billal <2643770+proafxin@users.noreply.github.com>
2021-03-24 05:23:49 +0600

    added generic view for get/post/put/patch/delete

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from rest_api.views import (
-    article_list,
     AuthorList,
     ArticleList,
+    ArticleDetail,
+    ArticleDetailGeneric,
+    ArticleDetailPost,
 )
 
 urlpatterns = [
     path('articles/', ArticleList.as_view(), name='articles'),
     path('authors/', AuthorList.as_view(), name='authors'),
+    path('article/<int:pk>', ArticleDetailGeneric.as_view(), name='article'),
+    path('article/', ArticleDetailPost.as_view(), name='article-post'),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -3,6 +3,7 @@ from django.http import (
     JsonResponse,
 )
 from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
@@ -10,11 +11,15 @@ from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
 )
 from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
 )
+from rest_framework.views import APIView
 from rest_framework.permissions import (
     IsAdminUser,
     IsAuthenticated,
@@ -69,3 +74,41 @@ class AuthorList(ListCreateAPIView):
         IsAuthenticated,
         IsAdminUser,
     ]
+
+class ArticleDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            article = Article.objects.get(pk=pk)
+
+            return article
+        except Article.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        response = Response(serializer.data)
+
+        return response
+    
+    def post(self, request, pk, format=None):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = serializer.data
+            status = HTTP_201_CREATED
+            response = Response(data=data, status=status)
+        else:
+            status = HTTP_400_BAD_REQUEST
+            response = Response(data=serializer.errors, status=status)
+        
+        return response
+
+class ArticleDetailGeneric(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetailPost(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
