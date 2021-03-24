6cab772af2c261c859719bec270627373c1e6832
Masum Billal <2643770+proafxin@users.noreply.github.com>
2021-03-24 09:59:45 +0600

    added unit testing for author get and post

--- rest_api/tests/test_unit.py
+++ rest_api/tests/test_unit.py
@@ -4,6 +4,7 @@ from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
     HTTP_403_FORBIDDEN,
     HTTP_404_NOT_FOUND,
 )
@@ -54,23 +55,106 @@ class TestAuthor(APITestCase):
             body='body big',
         )
     
-    def test_get_article_list(self):
+    def check_login(self):
         self.assertTrue(
             self.client.login(
                 username=USERNAME,
                 password=PASSWORD,
             )
         )
+    
+    def test_get_article_list(self):
+        self.check_login()
         url = 'articles'
         response = self.client.get(reverse(url))
         data = response.json()
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 3)
+        for i in range(len(data)):
+            self.assertEqual(data[i]['id'], i+1)
         self.assertEqual(data[2]['title'], 'title big')
         self.assertEqual(data[0]['title'], 'title1')
         self.assertEqual(data[1]['title'], 'title2')
-        self.assertEqual(len(data), 3)
+        self.client.logout()
         self.assertFalse(self.client.force_authenticate(user=None))
         response = self.client.get(reverse(url))
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
-        
+    
+    def test_get_article_detail(self):
+        self.check_login()
+        url = '/article/1'
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['title'], 'title1')
+        self.client.logout()
+        self.assertFalse(self.client.force_authenticate(user=None))
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, HTTP_200_OK)
+    
+    def test_post_article_detail(self):
+        self.check_login()
+        params = {
+            "title": "title new",
+            "author": 1,
+            "subject": "subject new",
+            "body": "body new",
+        }
+        url = reverse('article-post')
+        response = self.client.post(url, params)
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+        data = response.json()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['author'], 1)
+        self.assertEqual(data['id'], 4)
+        self.client.logout()
+        response = self.client.post(url, params)
+        self.assertNotEqual(response.status_code, HTTP_201_CREATED)
+    
+    def test_get_author_list(self):
+        self.check_login()
+        url = reverse('authors')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        data = response.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 2)
+        for i in range(len(data)): 
+            self.assertEqual(data[i]['id'], i+1)
+        self.client.logout()
+        self.client.force_authenticate(user=None)
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, HTTP_200_OK)
+    
+    def test_get_author_detail(self):
+        self.check_login()
+        url = '/author/2'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        data = response.json()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['id'], 2)
+        self.assertEqual(len(data['articles']), 2)
+        for x in data['articles']:
+            self.assertEqual(x['author'], 2)
+        self.client.logout()
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, HTTP_200_OK)
+    
+    def test_post_author_detail(self):
+        self.check_login()
+        url = reverse('author-post')
+        params = {
+            'name': 'test new',
+            'username': 'testname4',
+            'email': 'test4@test.com',
+        }
+        response = self.client.post(path=url, data=params)
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+        data = response.json()
+        self.assertEqual(data['id'], 3)
+        self.client.logout()
+        response = self.client.post(url, params)
+        self.assertNotEqual(response.status_code, HTTP_201_CREATED)
