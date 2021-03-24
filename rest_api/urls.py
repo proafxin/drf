e287bfac51478a505add9d9540296d231683efb4
Masum Billal <2643770+proafxin@users.noreply.github.com>
2021-03-24 06:14:27 +0600

    added author to rest api and fixed authentication

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from rest_api.views import (
     AuthorList,
     ArticleList,
-    ArticleDetail,
+    #ArticleDetail,
     ArticleDetailGeneric,
     ArticleDetailPost,
+    AuthorDetailPost,
+    AuthorDetailGeneric,
 )
 
 urlpatterns = [
@@ -13,4 +15,6 @@ urlpatterns = [
     path('authors/', AuthorList.as_view(), name='authors'),
     path('article/<int:pk>', ArticleDetailGeneric.as_view(), name='article'),
     path('article/', ArticleDetailPost.as_view(), name='article-post'),
+    path('author/', AuthorDetailPost.as_view(), name='author-post'),
+    path('author/<int:pk>', AuthorDetailGeneric.as_view(), name='author-post'),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -2,8 +2,7 @@ from django.http import (
     HttpResponse,
     JsonResponse,
 )
-from django.views.decorators.csrf import csrf_exempt
-from django.http import Http404
+#from django.http import Http404
 
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
@@ -23,7 +22,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import (
     IsAdminUser,
     IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
 )
 
 from rest_api.models import (
@@ -36,79 +34,83 @@ from rest_api.model_serializers import (
 )
 
 
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        data = serializer.data
-        json_response = JsonResponse(data=data, safe=False)
+# def article_list(request):
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         data = serializer.data
+#         json_response = JsonResponse(data=data, safe=False)
 
-        return json_response
+#         return json_response
     
-    if request.method == 'POST':
-        parser = JSONParser()
-        data = parser.parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            json_response = JsonResponse(serializer.data, status=201)
-        else:
-            json_response = JsonResponse(serializer.errors, status=400)
+#     if request.method == 'POST':
+#         parser = JSONParser()
+#         data = parser.parse(request)
+#         serializer = ArticleSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             json_response = JsonResponse(serializer.data, status=201)
+#         else:
+#             json_response = JsonResponse(serializer.errors, status=400)
         
-        return json_response
+#         return json_response
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = [
-        IsAdminUser,
-        IsAuthenticated,
-        IsAuthenticatedOrReadOnly,
-    ]
+    permission_classes = [IsAuthenticated]
 
 class AuthorList(ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = [
-        IsAuthenticatedOrReadOnly,
-        IsAuthenticated,
-        IsAdminUser,
-    ]
+    permission_classes = [IsAuthenticated]
 
-class ArticleDetail(APIView):
+# class ArticleDetail(APIView):
     
-    def get_object(self, pk):
-        try:
-            article = Article.objects.get(pk=pk)
+#     def get_object(self, pk):
+#         try:
+#             article = Article.objects.get(pk=pk)
 
-            return article
-        except Article.DoesNotExist:
-            raise Http404
+#             return article
+#         except Article.DoesNotExist:
+#             raise Http404
     
-    def get(self, request, pk, format=None):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(article)
-        response = Response(serializer.data)
+#     def get(self, request, pk, format=None):
+#         article = self.get_object(pk)
+#         serializer = ArticleSerializer(article)
+#         response = Response(serializer.data)
 
-        return response
+#         return response
     
-    def post(self, request, pk, format=None):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = serializer.data
-            status = HTTP_201_CREATED
-            response = Response(data=data, status=status)
-        else:
-            status = HTTP_400_BAD_REQUEST
-            response = Response(data=serializer.errors, status=status)
+#     def post(self, request, pk, format=None):
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             data = serializer.data
+#             status = HTTP_201_CREATED
+#             response = Response(data=data, status=status)
+#         else:
+#             status = HTTP_400_BAD_REQUEST
+#             response = Response(data=serializer.errors, status=status)
         
-        return response
+#         return response
 
 class ArticleDetailGeneric(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
 
 class ArticleDetailPost(CreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+
+class AuthorDetailGeneric(RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
+
+class AuthorDetailPost(CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
